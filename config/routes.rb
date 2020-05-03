0f0e97831e3af4948302417bcfab1641361f5198
Philip Davies <me@daviesp.co.uk>
2020-05-03 17:44:06 +0100

    finished video 14

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -7,6 +8,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :contacts
     resource :sessions, only: [:create, :destroy]
+    resources :users, only: [:create]
   end
-
 end
