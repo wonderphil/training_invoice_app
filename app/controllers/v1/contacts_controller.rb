6cbad48f2f1bd94dbbea1d1461f800f2cf980b02
Philip Davies <me@daviesp.co.uk>
2020-05-09 13:01:00 +0100

    finished video 19

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,30 +1,38 @@
 # Contacts controller
-class V1::ContactsController < ApplicationController
-  def index
-    @contacts = current_user.contacts
+module V1
+  class ContactsController < ApplicationController
+    def index
+      @contacts = current_account.contacts
 
-    render :index, status: :ok
-  end
+      render :index, status: :ok
+    end
 
-  def create
-    @contact = current_user.build(contact_params)
-    @contact.save
-    render :create, status: :created
-  end
+    def create
+      organization = current_account.organization.friendly.find(params[:organization_id])
+      @contact = organization.contacts.build(contact_params)
+      @contact.save
+      render :create, status: :created
+    end
 
-  def destroy
-    @contact = current_user.contact.where(id: params[:id]).first
+    def destroy
+      organization = current_account.organization.friendly.find(params[:organization_id])
+      @contact = organization.contact.where(id: params[:id]).first
 
-    if @contact.destroy
-      head(:ok)
-    else
-      head(:unprocessable_entity)
+      if @contact.destroy
+        head(:ok)
+      else
+        head(:unprocessable_entity)
+      end
+    end
+
+    private
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email)
+    end
+
+    def current_account
+      @account ||= Account.friendly.find(params[:account_id])
+      @account
     end
   end
-
-  private
-  def contact_params
-    params.require(:contact).permit(:first_name, :last_name, :email)
-  end
-
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,9 @@
 class Account < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+  
   belongs_to :owner, class_name: 'User'
+  
   has_many :organizations
+  has_many :contacts, through: :organizations
 end
