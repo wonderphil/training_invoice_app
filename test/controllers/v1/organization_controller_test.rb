f1861b5b3abf0d4cff39c617e85ec881f23cacdd
Philip Davies <me@daviesp.co.uk>
2020-05-14 20:40:10 +0100

    finished video 32

--- test/controllers/v1/organization_controller_test.rb
+++ test/controllers/v1/organization_controller_test.rb
@@ -30,7 +30,7 @@ module V1
       organization_params = {
         name: Faker::Company.name,
         tax_payer_number: Faker::Company.ein,
-        address: Faker::Address.street_name,
+        address: Faker::Address.street_name
       }
 
       post(
@@ -45,5 +45,22 @@ module V1
 
       assert organization['name'] == organization_params[:name]
     end
+
+    test 'shoud return unprocessable_entity' do 
+      organization_params = {
+        name: Faker::Company.name,
+        tax_payer_number: '',
+        address: ''
+      }
+
+      post(
+        v1_organizations_path(@account),
+        headers: @header,
+        params: { organization: organization_params }
+      )
+
+      assert_response :unprocessable_entity
+    end
+
   end
 end
