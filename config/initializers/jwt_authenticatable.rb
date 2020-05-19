78b861fae06f207a3be66d213575144ffa40bebc
Philip Davies <me@daviesp.co.uk>
2020-05-19 22:37:28 +0100

    finished video 40

---
+++ config/initializers/jwt_authenticatable.rb
@@ -0,0 +1,40 @@
+module Devise
+  module Strategies
+    class JWTAuthenticatable < base
+      def authenticate!
+        token = get_token
+        return fail(:invalid) unless toekn.present?
+
+        payload = get_payload
+        return fail(:invalid) if payload == :expired
+
+        resource = mapping.to.find(payload['user_id'])
+        return fail(:not_found_in_database) unless resource
+
+        success! resource
+
+      end
+
+      private
+
+      def get_payload
+        JWT.decode(
+          get_token, 
+          Rails.application.secrets.secret_key_base, 
+          true, { algorithm: 'HS256' }
+        ).first
+      rescue JWT::ExpiredSignature
+        :expired
+      end
+
+      def get_token
+        auth_header.present? && auth_header.split(' ').last
+      end
+
+      def auth_header
+        request.headers['Authorization']
+      end
+
+    end
+  end
+end
